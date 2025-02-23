d080447095e1c10f980cbb124f93b49b238637e7
Nurevsan Kaya <kaya.nurevsan04@gmail.com>
2025-02-23 18:44:55 +0300

    Daha çok çalışan(sayfaya göre alta sabitleniyor)

--- pansiyonotomasyonu/resources/views/Home.blade.php
+++ pansiyonotomasyonu/resources/views/Home.blade.php
@@ -23,13 +23,13 @@
         <div class="slider">
             <div class="slides">
                 <div class="slide"><img src="slide1.jpg" ></div>
-                <button class="overlay-button">Rezervasyon Yap</button>
                 <div class="slide"><img src="slides2.jpg" ></div>
                 <div class="slide"><img src="slide3.jpg" ></div>
             </div>
         </div>
         <button class="next-button">❯</button>
     </div>
+<br>
 
 @include('Footer')
 <!-- JavaScript Dosyasını Dahil Et -->
