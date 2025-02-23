5a956171d75df4dcc4a1704c38dd19b5e737a7d4
gderenc05 <cnkl.gldrn@gmail.com>
2025-02-23 21:01:12 +0300

    2.

--- pansiyonotomasyonu/routes/web.php
+++ pansiyonotomasyonu/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Home');
 });
+Route::get('/contact', function () {
+    return view('contact'); // contact.blade.php dosyasını döndürür
+});
+Route::get('/', function () {
+    return view('Slider');
+});
